8ed2e62ec98c43afcf2b63ccc821c6c523048b74
Coffee-Please <priya.singh.310@my.csun.edu>
2021-01-27 12:22:48 -0800

    Testing website start

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<!--
+ COMP 484 | Fall 2020
+ File: index.php
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Metadata -->
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<!-- Stylesheets and Favicon -->
+		<link rel="stylesheet" type="text/css" href="styles/root.css"/>
+		<link rel="stylesheet" type="text/css" href="styles/about.css"/>
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:wght@700&display=swap">
+		<link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
+
+		<!-- Website Title -->
+		<title>484 Chatroom</title>
+	</head>
+
+	<body>
+		<header>
+			<!-- Website Title Header -->
+			<div class="header-title">
+				<h1 class="main-header">COMP 484 | Web Enginnering</h1>
+				<h1 class="res-header">COMP 484 <br>Web Enginnering</h1>
+			</div>
+
+			<!-- Navigation Bar -->
+			<nav>
+				<a class="home" href="index.html">Home</a>
+				<a class="about" href="about.html">About</a>
+			</nav>
+		</header>
+
+		<main class="about-main">
+			<!-- About Title -->
+			<h3>
+				About
+			</h3>
+
+			<!-- About Description -->
+			<p class="about-text">
+				Forming effective study groups over long distances is not easy when taking online classes, especially if students do not take an initiative to establish communication to form study groups.
+				<br>
+				Therefore, a web application with a virtual chat room that allows students to connect and collaborate similarly to how they would in a study group would be beneficial.
+				<br>
+				<br>
+				We plan to integrate this chat room into our larger Senior Design project in COMP 490.
+			</p>
+		</main>
+	</body>
+
+	<footer>
+		<!-- Copyright Disclaimer -->
+		<p>&copy;2020 All rights reserved.</p>
+	</footer>
+</html>
