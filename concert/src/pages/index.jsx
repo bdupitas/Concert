2b91eb72df726e54f74cf58252abf091dc832e9a
Jonathan Lieu <jonathan.lieu.57@my.csun.edu>
2021-02-03 11:47:34 -0800

    create create-group.jsx pages

---
+++ concert/src/pages/index.jsx
@@ -0,0 +1,16 @@
+/*index.jsx*/
+//Import all needed Component for this tutorial
+import {Link} from "react-router-dom";
+
+//Functional Component
+const MainPage = () => {
+  return (
+    <div>
+      <h3>Login</h3>
+      <br/>
+      <Link to="/dash">Create Group</Link>
+    </div>
+  );
+};
+
+export default MainPage;
