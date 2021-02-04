d396eadd32cab59f6070dd037943381bfbff0daa
brandon <brandondupitas@gmail.com>
2021-02-04 12:47:59 -0800

    installed and updated npm packages

--- concert/src/App.js
+++ concert/src/App.js
@@ -1,9 +1,27 @@
+/*App.js*/
+import React from "react";
+import "./App.css";
 
-import './App.css';
+//Import all needed Component for this tutorial
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 
-function App() {
- return (<h1>hello world, .eslintcache sucks</h1>);
+//Pages
+import MainPage from "./pages/index"; ///< index.jsx will be automatically imported
+import GroupDash from "./pages/group-dash"; ///< index.jsx will be automatically imported
 
+const App = () => {
+    return (
+      <Router>
+      <Switch>
+       <Route exact path="/" component={MainPage}/>
+       <Route exact path="/dash" component={GroupDash}/>
+       </Switch>
+      </Router>
+    );
 }
 
 export default App;
---
+++ concert/src/pages/index.jsx
@@ -0,0 +1,10 @@
+
+import Login from "../components/Login"
+
+//Functional Component
+const MainPage = () => <div> <Login />  </div>
+      
+   
+
+
+export default MainPage;
