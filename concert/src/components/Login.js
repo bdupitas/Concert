61a2bcb26b6f9c0dbced4bdd71831b31f813ac85
brandon <brandondupitas@gmail.com>
2021-02-04 18:36:29 -0800

    added initial login boilerplate

--- concert/src/components/Login.js
+++ concert/src/components/Login.js
@@ -1,15 +1,42 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import react from "react"
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-
-const Login = () => {
-return(
-<>
-<h3>Login</h3>
-<br/>
-<Link to="/dash">Group Dash</Link>
-</>
-);
+const Login = (props) => {
+  return (
+    <Jumbotron>
+    <Container>
+      <Row>
+        <Col>test</Col>
+        <Col>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+            Submit
+             </Button>
+          </Form>
+        </Col>      
+      </Row>
+      </Container>
+    </Jumbotron>
+  );
 };
    
 
