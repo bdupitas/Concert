56c801c128d22b2e19d131f937faef040eb64e18
Coffee-Please <priya.singh.310@my.csun.edu>
2021-02-07 13:19:41 -0800

    - Installed packages for using `View` - Added components to taskbar - Converted some vanilla code to react

--- concert/src/components/taskbar.jsx
+++ concert/src/components/taskbar.jsx
@@ -1,30 +1,28 @@
 import "./../styles/taskbar.css";
 import "./../styles/root.css";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
 import TaskBarGroupIcons from "./../components/taskbar-group-icons";
 import CreateGroupButton from "./../components/create-group-button";
 import HomeButton from "./../components/home-button";
+import Container from 'react-bootstrap/Container';
+import { View } from 'react-native';
+
 
 function TaskBar () {
   return (
     <>
     <Container className="taskbar">
-      <Col>
-      <Row>
+      <View className="home-button">
           {/*<!-- Home Button -->*/}
           <HomeButton />
-      </Row>
-      <Row className="taskbar-group-icons">
+      </View>
+      <View className="taskbar-group-icons">
         {/*<!-- Created Groups -->*/}
-        <TaskBarGroupIcons/>
-      </Row>
-      <Row>
+        <TaskBarGroupIcons />
+      </View>
+      <View className="create-group-button">
         {/*<!-- Create Group Button -->*/}
         <CreateGroupButton />
-      </Row>
-      </Col>
+      </View>
     </Container>
     </>
   );
