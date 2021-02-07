4e379d1bc2f7f1a83fecaeed646da2d8804ad739
Jonathan Lieu <jonathan.lieu.57@my.csun.edu>
2021-02-07 00:01:51 -0800

    installed material-ui-styles and icons

--- concert/src/components/Login.js
+++ concert/src/components/Login.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Modal } from 'react-bootstrap'
 
 
 const Login = () => {
