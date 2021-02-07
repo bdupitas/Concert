271f0be6b282f7fad00f22d947ba17a8d0bf42bc
Jonathan Lieu <jonathan.lieu.57@my.csun.edu>
2021-02-06 21:10:31 -0800

    update login.js
    formatting

--- concert/src/components/Login.js
+++ concert/src/components/Login.js
@@ -1,15 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Modal } from 'react-bootstrap'
 
 
 const Login = () => {
-return(
-<>
-<h3>Login</h3>
-<br/>
-<Link to="/dash">Group Dash</Link>
-</>
-);
+
+    return(
+    <>
+        <h3>Login</h3>
+        <br/>
+        <Link to="/dash">Group Dash</Link>
+        <br/><br/>
+    </>
+    );
 };
    
 
