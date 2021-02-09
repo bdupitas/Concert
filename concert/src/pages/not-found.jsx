5abec4f80163929a497e5f0463ae0387330e4c61
Coffee-Please <priya.singh.310@my.csun.edu>
2021-02-08 20:54:32 -0800

    - Added 404 page

---
+++ concert/src/pages/not-found.jsx
@@ -0,0 +1,22 @@
+/// Image imports
+import { ReactComponent as Lotus } from "../images/lotus.svg";
+
+
+function NotFound () {
+  return (
+    <>
+    <div style={{ textAlign: 'center' }}>
+      <Lotus className="lotus-svg" />
+          <p style={{ fontSize: '2em' }}>404</p>
+      <p>You look lost, why not hang around here for a bit?</p>
+      <footer>
+        <div>Icons made by
+          <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
+      </footer>
+    </div>
+    </>
+  );
+}
+
+export default NotFound;
