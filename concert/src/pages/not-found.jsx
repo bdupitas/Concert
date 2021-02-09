6ca8222ae8652ebd62390c099de216ce4561373a
Coffee-Please <priya.singh.310@my.csun.edu>
2021-02-08 21:06:26 -0800

    - Font adjustment

--- concert/src/pages/not-found.jsx
+++ concert/src/pages/not-found.jsx
@@ -10,7 +10,7 @@ function NotFound () {
           <p style={{ fontSize: '2em' }}>404</p>
       <p>You look lost, why not hang around here for a bit?</p>
       <footer>
-        <div>Icons made by
+        <div style={{ fontSize: '0.7em' }}>Icons made by
           <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
       </div>
       </footer>
