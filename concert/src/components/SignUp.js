49d88d586dc97cf5562f8460466c94a5eafc547e
brandon <brandondupitas@gmail.com>
2021-02-15 20:03:11 -0800

    added college names

--- concert/src/components/SignUp.js
+++ concert/src/components/SignUp.js
@@ -5,6 +5,19 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { ReactComponent as Chalkboard } from "../images/createChalkboard.svg";
 
+const collegeNames = [
+  "Mike Curb College of Arts, Media, & Communication",
+  "David Nazarian College of Business and Economics",
+  "Michael D. Eisner College of Education",
+  "Engineering & Computer Science",
+  "Health & Human Development",
+  "Humanities",
+  "Oviatt Library",
+  "Science & Mathematics",
+  "Social & Behavioral Sciences",
+  "The Tseng College"
+];
+
 const SignUp = ({ children }) => {
   return (
     <>
@@ -19,13 +32,11 @@ const SignUp = ({ children }) => {
                 <Form.Label>First Name</Form.Label>
                 <Form.Control type="email" placeholder="" />
               </Form.Group>
-
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control type="password" placeholder="" />
               </Form.Group>
             </Form.Row>
-
             <Form.Group controlId="formGridAddress1">
               <Form.Label>Email Address</Form.Label>
               <Form.Control placeholder="" />
@@ -40,6 +51,37 @@ const SignUp = ({ children }) => {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="" />
             </Form.Group>
+            <Form.Row className="align-items-center">
+              <Col xs="auto" className="my-1">
+                <Form.Label
+                  className="mr-sm-2"
+                  htmlFor="inlineFormCustomSelect"
+                  srOnly
+                >
+                  Preference
+                </Form.Label>
+                <Form.Control
+                  as="select"
+                  className="mr-sm-2"
+                  id="inlineFormCustomSelect"
+                  custom
+                >
+
+                {collegeNames.map((college, i) =>
+                <option value={i}>{college}</option>
+                )}
+                
+                </Form.Control>
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Form.Check
+                  type="checkbox"
+                  id="customControlAutosizing"
+                  label="I am a Tutor"
+                  custom
+                />
+              </Col>
+            </Form.Row>
 
             <Button variant="primary" type="submit">
               Create Account
