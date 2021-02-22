7e00b722d596e393d4c886b81d81d3c318341d82
Coffee-Please <priya.singh.310@my.csun.edu>
2021-02-22 00:46:02 -0800

    Added Additional buttons for editing info. Edit info toggles readonly property of select fields. Was wondering if edit info was necessary since we'll be checking against existing student database? Added modal for password change success, but it isn't hooked up.

--- concert/src/pages/account.jsx
+++ concert/src/pages/account.jsx
@@ -1,20 +1,27 @@
 /// Image imports
 import { ReactComponent as Star } from "./../images/star.svg";
-import React, { useState } from "react";
+import { ReactComponent as Check } from "./../images/check-mark.svg";
+
+// CSS imports
+import "./../styles/root.css";
+import "./../styles/account.css";
+
+// Component imports
+import React, { useState, setState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
-import "./../styles/root.css";
-import "./../styles/account.css";
 import TaskBar from "./../components/taskbar";
 import Modal from 'react-bootstrap/Modal'
 
-/// TODO: Edit info UI functionality?
-/// TODO: Change PW Success Modal
-/// TODO: Optimize CSS
 
+/// TODO: Optimize CSS
+/// TODO: Spilt into components
+/// TODO: Conditionally render components (buttons) for edit info
+
+/// Pop-up for account deletion
 function DeleteAccountModal(props) {
   return (
     <Modal
@@ -41,7 +48,7 @@ function DeleteAccountModal(props) {
   );
 }
 
-
+/// Pop-up for changing password
 function ChangePasswordModal(props) {
   return (
     <Modal
@@ -83,8 +90,41 @@ function ChangePasswordModal(props) {
   );
 }
 
+/// Pop-up success message for successful password change
+/// TODO: Change CSS for PW Success Modal
+/// TODO: Hook up to password modal or set to pop-up on submit success
+function passwordSuccess(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="pws-contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="pws-contained-modal-title-vcenter">
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* Image */}
+        <Check className="check-svg" />
+        Password was Successfully Changed!
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 
 function UserAccount () {
+
+  // Hooks
+  const [readState, setReadState] = React.useState("readOnly"); // Read only toggle
+  const [initialBtnState, setInitialBtnState] = React.useState(true); // Initial Button group
+  const [editBtnState, seteditBtnState] = React.useState(false); // Edit Info button group
+
   const [modalShow1, setModalShow1] = React.useState(false); // Change PW Modal
   const [modalShow2, setModalShow2] = React.useState(false); // Delete Account Modal
 
@@ -112,20 +152,20 @@ function UserAccount () {
               <Form.Row>
                 <Form.Group as={Col} controlId="formGridFname">
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="text" placeholder="FNAME" readOnly/>
+                  <Form.Control type="text" placeholder="FNAME" readOnly={readState}/>
                 </Form.Group>
 
                 {/* LNAME */}
                 <Form.Group as={Col} controlId="formGridLname">
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="text" placeholder="LNAME" readOnly/>
+                  <Form.Control type="text" placeholder="LNAME" readOnly={readState}/>
                 </Form.Group>
               </Form.Row>
 
               {/* EMAIL */}
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" readOnly/>
+                <Form.Control type="email" placeholder="Enter email" readOnly={readState}/>
               </Form.Group>
 
               {/* PASSWORD */}
@@ -133,6 +173,7 @@ function UserAccount () {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" readOnly/>
               </Form.Group>
+
               <Row>
                 <Col>
                   {/* Buttons */}
@@ -140,9 +181,24 @@ function UserAccount () {
                     className="btn-group-vertical"
                     aria-label="Toolbar with Button groups"
                   >
+
+                  <div>
+                    {/* Submit Changes */}
+                    <Button variant="primary" style={{margin: "1em 0em"}}>
+                      Submit Changes
+                    </Button>
+                  </div>
+
+                  <div>
+                    {/* Submit Changes */}
+                    <Button variant="outline-primary" style={{margin: "1em 0em"}}>
+                      Cancel
+                    </Button>
+                  </div>
+
                   <div>
                     {/* EDIT INFO */}
-                      <Button variant="primary" style={{margin: "1em 0em"}}>
+                      <Button variant="primary" style={{margin: "1em 0em"}} onClick={() => readState == "readOnly" ? setReadState("") : setReadState("readOnly")}>
                         Edit Information
                       </Button>
                   </div>
@@ -167,11 +223,13 @@ function UserAccount () {
               </Row>
             </Form>
           </Col>
+
           <Col className="img-container">
             {/* Image */}
             <Star className="star-svg" />
           </Col>
         </Row>
+
       </div>
       </div>
     </>
